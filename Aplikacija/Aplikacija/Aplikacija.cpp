8269a5942c9d1103c73f67b220547ae426b466b2
Kristijan <kbirtic@etfos.hr>
2016-04-12 13:39:08 +0200

    prvicommit

---
+++ Aplikacija/Aplikacija/Aplikacija.cpp
@@ -0,0 +1,12 @@
+// Aplikacija.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Pozdrav iz aplikacije od Kristijan Birtic");
+	return 0;
+}
+
